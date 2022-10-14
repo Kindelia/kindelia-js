5722e074a44151fb0b6588f03cf7c3514172c58d
Rodrigo Oler <roodrigoprogrammer@gmail.com>
2022-10-14 18:14:46 -0300

    chore: create types

--- src/types.ts
+++ src/types.ts
@@ -1,282 +1,282 @@
-import { Enum, Option, Tagged } from 'rustie'
+import { Enum, Option, Tagged } from "rustie";
 
-export type StrNum = Tagged<'StrNum', string>
-export type Name = Tagged<'Name', string>
-export type HashHex = Tagged<'Hash', string>
-
-export type BlockId = HashHex
-export type FunctionId = Name
-
-export type Result<T, E> = Enum<Result_Variants<T, E>>
+export type Result<T, E> = Enum<Result_Variants<T, E>>;
 export interface Result_Variants<T, E> {
-  Ok: T
-  Err: E
+  Ok: T;
+  Err: E;
 }
 
-// Raw API data
-// ============
+export type StrNum = Tagged<"StrNum", string>;
+export type Name = Tagged<"Name", string>;
+
+export type HashHex = Tagged<"Hash", string>;
+
+export type FunctionId = Name;
+
+export const checkHashHex = (hash: string): hash is HashHex =>
+  new RegExp("^(0x)?[0-9a-f]{64}$", "i").test(hash);
 
 export interface BlockInfoJson {
-  block: BlockJson
-  hash: HashHex
-  height: number
-  content: BlockContentJson
-  results: Option<BlockResultsJson[]>
+  block: BlockJson;
+  hash: HashHex;
+  height: number;
+  content: BlockContentJson;
+  results: Option<BlockResultsJson[]>;
 }
 
 export interface BlockJson {
-  time: string
-  rand: string
-  prev: string
-  body: number[]
+  time: string;
+  rand: string;
+  prev: string;
+  body: number[];
 }
 
-export type BlockContentJson = StatementJson[]
+export type BlockContentJson = StatementJson[];
 
-export type StatementJson = Enum<StatementJson_Variants>
+export type StatementJson = Enum<StatementJson_Variants>;
 export interface StatementJson_Variants {
-  Ctr: StmtCtrJson
-  Fun: StmtFunJson
-  Run: StmtRunJson
+  Ctr: StmtCtrJson;
+  Fun: StmtFunJson;
+  Run: StmtRunJson;
 }
 
 export interface StmtCtrJson {
-  name: Name
-  args: Name[]
+  name: Name;
+  args: Name[];
 }
 export interface StmtFunJson {
-  name: Name
-  args: Name[]
-  func: RuleJson[]
-  init: TermJson
+  name: Name;
+  args: Name[];
+  func: RuleJson[];
+  init: TermJson;
 }
 
 export interface FuncJson {
   func: {
-    rules: RuleJson[]
-  }
+    rules: RuleJson[];
+  };
 }
 export interface StmtRunJson {
-  body: TermJson
+  body: TermJson;
 }
 
 export interface RuleJson {
-  lhs: TermJson
-  rhs: TermJson
+  lhs: TermJson;
+  rhs: TermJson;
 }
 
-export type TermJson = Enum<TermJson_Variants>
+export type TermJson = Enum<TermJson_Variants>;
 export interface TermJson_Variants {
-  Var: VarJson
-  Dup: DupJson
-  Lam: LamJson
-  App: AppJson
-  Ctr: CtrJson
-  Fun: FunJson
-  Num: NumJson
-  Op2: Op2Json
+  Var: VarJson;
+  Dup: DupJson;
+  Lam: LamJson;
+  App: AppJson;
+  Ctr: CtrJson;
+  Fun: FunJson;
+  Num: NumJson;
+  Op2: Op2Json;
 }
 
 export interface VarJson {
-  name: Name
+  name: Name;
 }
 
 export interface DupJson {
-  nam0: Name
-  nam1: Name
-  expr: TermJson
-  body: TermJson
+  nam0: Name;
+  nam1: Name;
+  expr: TermJson;
+  body: TermJson;
 }
 
 export interface LamJson {
-  name: Name
-  body: TermJson
+  name: Name;
+  body: TermJson;
 }
 
 export interface AppJson {
-  func: TermJson
-  argm: TermJson
+  func: TermJson;
+  argm: TermJson;
 }
 
 export interface CtrJson {
-  name: Name
-  args: TermJson[]
+  name: Name;
+  args: TermJson[];
 }
 
 export interface FunJson {
-  name: Name
-  args: TermJson[]
+  name: Name;
+  args: TermJson[];
 }
 
 export interface Op2Json {
-  oper: StrNum
-  val0: TermJson
-  val1: TermJson
+  oper: StrNum;
+  val0: TermJson;
+  val1: TermJson;
 }
 
 export interface NumJson {
-  numb: StrNum
+  numb: StrNum;
 }
 
 // Block Results
 // -------------
 
-export type BlockResultsJson = Result<StatementInfoJson, { err: string }>
+export type BlockResultsJson = Result<StatementInfoJson, { err: string }>;
 export type BlockResultsJson_Variants = Result_Variants<
   StatementInfoJson,
   { err: string }
->
+>;
 
-export type StatementInfoJson = Enum<StatementInfoJson_Variants>
+export type StatementInfoJson = Enum<StatementInfoJson_Variants>;
 export interface StatementInfoJson_Variants {
-  Ctr: StmtCtrInfoJson
-  Fun: StmtFunInfoJson
-  Run: StmtRunInfoJson
+  Ctr: StmtCtrInfoJson;
+  Fun: StmtFunInfoJson;
+  Run: StmtRunInfoJson;
 }
 
 export interface StmtCtrInfoJson {
-  name: Name
-  args: Name[]
+  name: Name;
+  args: Name[];
 }
 
 export interface StmtFunInfoJson {
-  name: Name
-  args: Name[]
+  name: Name;
+  args: Name[];
 }
 
 export interface StmtRunInfoJson {
-  done_term: TermJson
-  end_size: StrNum
-  size_diff: StrNum
-  used_mana: StrNum
+  done_term: TermJson;
+  end_size: StrNum;
+  size_diff: StrNum;
+  used_mana: StrNum;
 }
 
 // Kindelia Types
 // ==============
 
 export interface Block {
-  time: Date
-  rand: null
-  prev: HashHex
-  body: null
+  time: Date;
+  rand: null;
+  prev: HashHex;
+  body: null;
 }
 
-export type BlockContent = Statement[]
+export type BlockContent = Statement[];
 
 export interface Statement_Variants {
-  Ctr: StmtCtr
-  Fun: StmtFun
-  Run: StmtRun
+  Ctr: StmtCtr;
+  Fun: StmtFun;
+  Run: StmtRun;
 }
 
-export type Statement = Enum<Statement_Variants>
+export type Statement = Enum<Statement_Variants>;
 
 export interface StmtCtr {
-  name: Name
-  args: Name[]
+  name: Name;
+  args: Name[];
 }
 export interface StmtFun {
-  name: Name
-  args: Name[]
-  func: Rule[]
-  init: Term
+  name: Name;
+  args: Name[];
+  func: Rule[];
+  init: Term;
 }
 export interface StmtRun {
-  body: Term
+  body: Term;
 }
 
 export interface Rule {
-  lhs: Term
-  rhs: Term
+  lhs: Term;
+  rhs: Term;
 }
 
 export interface Term_Variants {
-  Var: Var
-  Dup: Dup
-  Lam: Lam
-  App: App
-  Ctr: Ctr
-  Fun: Fun
-  Num: Num
-  Op2: Op2
-  IO: IO
+  Var: Var;
+  Dup: Dup;
+  Lam: Lam;
+  App: App;
+  Ctr: Ctr;
+  Fun: Fun;
+  Num: Num;
+  Op2: Op2;
+  IO: IO;
 }
-export type Term = Enum<Term_Variants>
+export type Term = Enum<Term_Variants>;
 
 export interface Var {
-  name: Name
+  name: Name;
 }
 
 export interface Dup {
-  nam0: Name
-  nam1: Name
-  expr: Term
-  body: Term
+  nam0: Name;
+  nam1: Name;
+  expr: Term;
+  body: Term;
 }
 
 export interface Lam {
-  name: Name
-  body: Term
+  name: Name;
+  body: Term;
 }
 
 export interface App {
-  func: Term
-  argm: Term
+  func: Term;
+  argm: Term;
 }
 
 export interface Ctr {
-  name: Name
-  args: Term[]
+  name: Name;
+  args: Term[];
 }
 
 export interface Fun {
-  name: Name
-  args: Term[]
+  name: Name;
+  args: Term[];
 }
 
 export interface Num {
-  numb: bigint
+  numb: bigint;
 }
 
 export interface Op2 {
-  oper: bigint // TODO: parse this tag
-  val0: Term
-  val1: Term
+  oper: bigint; // TODO: parse this tag
+  val0: Term;
+  val1: Term;
 }
 
 export interface IO_Variants {
-  Call: Call
-  Take: Take
-  Save: Save
-  Load: Load
-  Done: Done
+  Call: Call;
+  Take: Take;
+  Save: Save;
+  Load: Load;
+  Done: Done;
 }
 
-export type IO = Enum<IO_Variants>
+export type IO = Enum<IO_Variants>;
 
 export interface Call {
-  func: Name
-  args: Term[]
-  varv: Name
-  body: IO
+  func: Name;
+  args: Term[];
+  varv: Name;
+  body: IO;
 }
 
 export interface Take {
-  varv: Name
-  body: IO
+  varv: Name;
+  body: IO;
 }
 
 export interface Save {
-  term: Term
-  varv: Name
-  body: IO
+  term: Term;
+  varv: Name;
+  body: IO;
 }
 
 export interface Load {
-  varv: Name
-  body: IO
+  varv: Name;
+  body: IO;
 }
 
 export interface Done {
-  term: Term
+  term: Term;
 }
